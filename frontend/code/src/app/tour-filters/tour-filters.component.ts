b45e40045bc369bece9cd9def8124894c12af65e
Luciano Rodriguez <lucianorod1995@gmail.com>
2018-11-26 23:00:29 -0300

    Actualizo el frontend para agregar 4 filtros mas

--- frontend/code/src/app/tour-filters/tour-filters.component.ts
+++ frontend/code/src/app/tour-filters/tour-filters.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { TourSolverService } from '../tour-solver.service';
 import { TourResult } from '../tour-result/TourResult';
 import { tap } from 'rxjs/operators';
-import { TourRequest, OnCityFilter } from './TourRequest';
+import { TourRequest, OnCityFilter, GlobalFilter, OnTripFilter } from './TourRequest';
 
 @Component({
   selector: 'app-tour-filters',
@@ -14,12 +14,22 @@ export class TourFiltersComponent implements OnInit {
   @Output() search: EventEmitter<TourResult> = new EventEmitter<TourResult>();
   tourRequest: TourRequest = new TourRequest();
   citiesFilters = {};
+  globalFilters = [];
+  connections = [];
+  maxDuration = '';
   constructor(
     private tourSolverService: TourSolverService
   ) { }
 
   ngOnInit() {
     this.addPlace();
+    // this.tourRequest.src = 'Buenos Aires'
+    // this.tourRequest.toVisit = ['Paris','Barcelona','Londres']
+    // this.tourRequest.vacationDays = 7
+    // this.tourRequest.startDate = new Date(2019,0,1)
+    // this.citiesFilters = {0: [new OnCityFilter('Stay in city for at least', 'Paris', 3)], 
+    //   1: [],
+    //   2: []}
   }
 
   buscar() {
@@ -31,6 +41,19 @@ export class TourFiltersComponent implements OnInit {
       }
     }
 
+    this.tourRequest.filters['global'] = []
+    for (const globalFilter of this.globalFilters) {
+      if (globalFilter.name === 'Duration') {
+        const time = this.maxDuration.split(':')
+        globalFilter.max = parseInt(time[0])*60*60+parseInt(time[1])*60+parseInt(time[2])
+      }
+      this.tourRequest.filters['global'].push(globalFilter);
+    }
+
+    this.tourRequest.filters['ontrip'] = []
+    for (const ontripFilter of this.connections) {
+      this.tourRequest.filters['ontrip'].push(ontripFilter);
+    }
     this.tourSolverService.getTourResult(this.tourRequest)
       .pipe(
         tap(tour => console.log(tour))
@@ -47,6 +70,13 @@ export class TourFiltersComponent implements OnInit {
     this.citiesFilters[index].push(new OnCityFilter('Stay in city for at least', null, null));
   }
 
+  addGlobalFilter(name: string) {
+    this.globalFilters.push(new GlobalFilter(name, null))
+  }
+
+  addConnection() {
+    this.connections.push(new OnTripFilter('Connection', null, null, null))
+  }
   trackByFn(index: any, item: any) {
     return index;
   }
